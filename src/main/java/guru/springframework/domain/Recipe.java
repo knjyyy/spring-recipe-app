eeab86eaac2bb77d60a021c79b24f5f5489cf9e5
knjyyy <rzzlknjtrndd@gmail.com>
2019-10-15 19:33:35 +0800

    1. Added constructor for Ingredient (Description, Quantity, UOM). 2. Added method in Recipe to auto-assign recipe to Ingredient. 3. Used new methods to refactor the bootstrap process.

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -19,116 +19,146 @@ import javax.persistence.OneToOne;
 
 @Entity
 public class Recipe {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@JoinColumn
 	private Long id;
-	
+
 	private String description;
 	private int prepTime;
 	private int cookTime;
 	private int servings;
 	private String source;
 	private String url;
-	
+
 	@Lob
 	private String direction;
-	//todo add Dificulty
-	
+	// todo add Dificulty
+
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
 	private Set<Ingredient> ingredients = new HashSet<>();
-	
+
 	@Lob
 	private byte[] image;
-	
+
 	@Enumerated(value = EnumType.STRING)
 	private Difficulty difficulty;
-	
+
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
-	
+
 	@ManyToMany
-	@JoinTable(name = "recipe_category", 
-	joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	@JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public int getPrepTime() {
 		return prepTime;
 	}
+
 	public void setPrepTime(int prepTime) {
 		this.prepTime = prepTime;
 	}
+
 	public int getCookTime() {
 		return cookTime;
 	}
+
 	public void setCookTime(int cookTime) {
 		this.cookTime = cookTime;
 	}
+
 	public int getServings() {
 		return servings;
 	}
+
 	public void setServings(int servings) {
 		this.servings = servings;
 	}
+
 	public String getSource() {
 		return source;
 	}
+
 	public void setSource(String source) {
 		this.source = source;
 	}
+
 	public String getUrl() {
 		return url;
 	}
+
 	public void setUrl(String url) {
 		this.url = url;
 	}
+
 	public String getDirection() {
 		return direction;
 	}
+
 	public void setDirections(String direction) {
 		this.direction = direction;
 	}
+
 	public byte[] getImage() {
 		return image;
 	}
+
 	public void setImage(byte[] image) {
 		this.image = image;
 	}
+
 	public Notes getNotes() {
 		return notes;
 	}
+
 	public void setNotes(Notes notes) {
 		this.notes = notes;
+		notes.setRecipe(this);
 	}
+
 	public Set<Ingredient> getIngredients() {
 		return ingredients;
 	}
+
 	public void setIngredients(Set<Ingredient> ingredients) {
 		this.ingredients = ingredients;
 	}
+
+	public Recipe addIngredient(Ingredient ingredient) {
+		ingredient.setRecipe(this);
+		this.ingredients.add(ingredient);
+		return this;
+	}
+
 	public Difficulty getDifficulty() {
 		return difficulty;
 	}
+
 	public void setDifficulty(Difficulty difficulty) {
 		this.difficulty = difficulty;
 	}
+
 	public Set<Category> getCategories() {
 		return categories;
 	}
+
 	public void setCategories(Set<Category> categories) {
 		this.categories = categories;
 	}

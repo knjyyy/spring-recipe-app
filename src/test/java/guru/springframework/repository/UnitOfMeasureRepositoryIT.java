4cd565034311d37f6f3047ea44ce7d76dee86b2f
knjyyy <rzzlknjtrndd@gmail.com>
2019-10-21 14:33:07 +0800

    1. Added Integration Testing in UOM model. 2. Updated dependencies to include failsafe plugin.

---
+++ src/test/java/guru/springframework/repository/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,40 @@
+package guru.springframework.repository;
+
+import static org.junit.Assert.*;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import guru.springframework.domain.UnitOfMeasure;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+	@Autowired
+	UnitOfMeasureRepository unitOfMeasureRepository;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void findByDescriptionTeaspoon() {
+		String description = "Teaspoon";
+		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription(description);
+		assertEquals(description, uomOptional.get().getDescription());
+	}
+
+	@Test
+	public void findByDescriptionCup() {
+		String description = "Cup";
+		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription(description);
+		assertEquals(description, uomOptional.get().getDescription());
+	}
+}
